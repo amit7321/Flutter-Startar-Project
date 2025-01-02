3f9103bf0b72594ed14975164583f2dc327c42cd
amit7321 <amit.hasan7321@gmail.com>
2025-01-02 20:24:21 +0600

    Implement some widgets.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
-void main() {
-  runApp(MaterialApp());
-}
 
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.cyan, Colors.greenAccent],
+            ),
+          ),
+          child: const Center(
+            child: Text("Hello World"),
+          ),
+        ),
+      ),
+    ),
+  );
+}
